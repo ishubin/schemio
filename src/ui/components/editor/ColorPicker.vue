eaf8696730dc7b9fa4ebbc9bd24e2460bd2d001c
Ivan Shubin <ivan.ishubin@gmail.com>
2018-12-07 22:04:59 +0100

    Added ok and cancel buttons to color picker

--- src/ui/components/editor/ColorPicker.vue
+++ src/ui/components/editor/ColorPicker.vue
@@ -2,15 +2,21 @@
     <div class="color-picker">
         <span class="color-picker-toggle-button"
             :style="{'background': pickerColor}"
-            @click="toggleColorPicker = !toggleColorPicker"
+            @click="toggleColorPicker()"
             ></span>
 
 
-        <transition name="modal" v-if="toggleColorPicker">
-            <div class="modal-mask modal-mask-transparent" @click="toggleColorPicker = false">
+        <transition name="modal" v-if="showColorPicker">
+            <div class="modal-mask modal-mask-transparent">
                 <div class="modal-wrapper">
-                    <div class="modal-container" style="width: 200px; padding: 0;">
-                        <chrome-picker v-model="chromePickerColor" @input="updateColor"></chrome-picker>
+                    <div class="modal-container" style="width: 300px; background: #fff;">
+                        <div class="modal-body">
+                            <chrome-picker v-model="chromePickerColor" @input="updateColor"></chrome-picker>
+                        </div>
+                        <div class="modal-body">
+                            <span class="btn btn-primary" v-on:click="applyColor()">Ok</span>
+                            <span class="btn btn-secondary" v-on:click="cancelColorPicker()">Cancel</span>
+                        </div>
                     </div>
                 </div>
            </div>
@@ -28,13 +34,27 @@ export default {
         return {
             pickerColor: this.color,
             chromePickerColor: {hex: this.color},
-            toggleColorPicker: false
+            showColorPicker: false,
+            oldColor: null
         }
     },
     methods: {
+        toggleColorPicker() {
+            this.oldColor = this.color;
+            this.chromePickerColor = {hex: this.color};
+            this.showColorPicker = true;
+        },
         updateColor(color) {
             this.pickerColor = color.hex;
             this.$emit('input', color.hex);
+        },
+        applyColor() {
+            this.showColorPicker = false;
+        },
+        cancelColorPicker() {
+            this.pickerColor = this.oldColor;
+            this.$emit('input', this.oldColor);
+            this.showColorPicker = false;
         }
     },
     watch: {
