a60058e9313574ac2b595c87ffb407e087f982f1
Ivan Shubin <ivan.ishubin@gmail.com>
2018-12-15 21:40:57 +0100

    refactored image upload

--- src/server/api/apiImages.js
+++ src/server/api/apiImages.js
@@ -1,10 +1,27 @@
 const fs                = require('fs-extra');
+const multer            = require('multer');
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Math.random().toString(36).substring(2) + '-' + file.originalname);
+    }
+});
+const upload            = multer({storage}).single('image');
 
 module.exports = {
     uploadImage(req, res) {
-        res.json({
-            path: `/api/images/${req.file.filename}`
+        upload(req, res, err => {
+            if (!err) {
+                res.json({
+                    path: `/api/images/${req.file.filename}`
+                });
+            } else {
+                res.status(500);
+                res.json({error: 'Could not upload file'});
+            }
         });
     },
     getImage(req, res) {
