5956288fb915b16a367fc187caea93fa03512018
Ivan Shubin <ivan.ishubin@gmail.com>
2018-12-20 20:16:53 +0100

    Added links in item details

--- src/ui/components/editor/ItemDetails.vue
+++ src/ui/components/editor/ItemDetails.vue
@@ -3,6 +3,21 @@
         <h3>{{item.name}}</h3>
 
         <vue-markdown>{{item.description}}</vue-markdown>
+
+        <div v-if="item.links && item.links.length > 0">
+            <h5>Links</h5>
+            <ul class="links">
+                <li v-for="(link, linkId) in item.links">
+                    <a class="link" :href="link.url" target="_blank">
+                       <i v-if="link.type === 'graphs'" class="fas fa-chart-line"></i>
+                       <i v-if="link.type === 'logs'" class="fas fa-stream"></i>
+                       <i v-if="link.type === 'scheme'" class="fas fa-project-diagram"></i>
+                       <i v-if="link.type === 'default'" class="fas fa-link"></i>
+                        {{link.title}}
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
