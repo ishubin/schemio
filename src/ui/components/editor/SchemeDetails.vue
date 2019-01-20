891242fd0c0f8fea0c6fb0d3e0bc0fcc94d96ed3
Ivan Shubin <ivan.ishubin@gmail.com>
2019-01-20 12:05:52 +0100

    fixed category breadcrumb

--- src/ui/components/editor/SchemeDetails.vue
+++ src/ui/components/editor/SchemeDetails.vue
@@ -2,11 +2,11 @@
     <div>
         <ul class="category-breadcrumb" v-if="schemeContainer.scheme.category">
             <li v-for="category in schemeContainer.scheme.category.ancestors">
-                <a href="">{{category.name}}</a>
+                <a :href="'/?category=' + category.id">{{category.name}}</a>
                 <i class="fas fa-angle-right"></i>
             </li>
             <li>
-                <a href="">{{schemeContainer.scheme.category.name}}</a>
+                <a :href="'/?category=' + schemeContainer.scheme.category.id">{{schemeContainer.scheme.category.name}}</a>
             </li>
         </ul>
         <h3>{{schemeContainer.scheme.name}}</h3>
