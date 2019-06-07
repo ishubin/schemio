283c802e0e5516ddf1f14dfdc114597add5f2d9d
Ivan Shubin <ivan.ishubin@gmail.com>
2019-06-07 22:25:39 +0200

    Implemented fake demo storage manipulation from browser

--- src/ui/localStorageDb.js
+++ src/ui/localStorageDb.js
@@ -4,11 +4,86 @@
 
 import _ from 'lodash';
 
+const localStorageClientFunctions = {
+    exportAllStorage() {
+        let collectedData = {};
+        let promises = []
+        _.forEach(document.schemioLocalStorageDatabases, (db, dbName) => {
+            promises.push(db.find().then(items => {
+                collectedData[dbName] = items;
+            }));
+        });
+
+        Promise.all(promises).then(() => {
+            const newWindow = window.open();
+            newWindow.document.write(JSON.stringify(collectedData, 4));
+        });
+    },
+
+    importAllStorage() {
+        let html = '<div style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: #fff;">';
+        html += '<button id="schemio-local-storage-import-submit">Import</button><br/>';
+        html += '<textarea id="schemio-local-storage-import-input" style="width: 100%; height: 400px; box-sizing: border-box;"></textarea>';
+        html += '</div>;'
+        document.body.innerHTML += html;
+
+        document.getElementById('schemio-local-storage-import-submit').addEventListener('click', () => {
+            let inputText = document.getElementById('schemio-local-storage-import-input').value;
+            const jsonToImport = JSON.parse(inputText);
+            
+            let chain = Promise.resolve(null);
+            _.forEach(document.schemioLocalStorageDatabases, (db, dbName) => {
+                if (jsonToImport[dbName]) {
+                    _.forEach(jsonToImport[dbName], doc => {
+                        chain = chain.then(() => {
+                            db.save(doc.id, doc);
+                        });
+                    });
+                }
+            });
+
+            chain.then(() => window.location.reload());
+        });
+    },
+
+    clearStorage() {
+        let promises = []
+        _.forEach(document.schemioLocalStorageDatabases, (db, dbName) => {
+            promises.push(db.clear());
+        });
+
+        Promise.all(promises).then(() => {
+            window.location.reload();
+        });
+    }
+};
+
+
+
+if (!document.schemioLocalStorage) {
+    document.schemioLocalStorageDatabases = {};
+    document.schemioLocalStorage = localStorageClientFunctions;
+}
+
 
 export default class LocalStorageDb {
+    /**
+     * 
+     * @param {string} collectionName 
+     */
     constructor(collectionName) {
         this.collectionName = collectionName;
         this.index = this._get(`ldb-${this.collectionName}-index`, {});
+        if (!this._get(`ldb-${this.collectionName}-index`)) {
+            this._initStorage();
+        }
+        document.schemioLocalStorageDatabases[this.collectionName] = this;
+    }
+
+    /**
+     * Initializes storage with local data
+     */
+    _initStorage() {
     }
 
     _set(fullId, data) {
@@ -26,6 +101,17 @@ export default class LocalStorageDb {
 
     _delete(fullId) {
         window.localStorage.removeItem(fullId);
+    }
+
+    clear() {
+        let promises = [];
+        _.forEach(this.index, id => {
+            promises.push(this.delete(id));
+        });
+
+        Promise.all(promises).then(() => {
+            this._delete(`ldb-${this.collectionName}-index`);
+        });
     }
 
     /**
@@ -84,8 +170,8 @@ export default class LocalStorageDb {
         if (this.index[id]) {
             delete this.index[id];
             this._set(`ldb-${this.collectionName}-index`, this.index);
-            this._delete(`ldb-${this.collectionName}-document-${id}`);
         }
+        this._delete(`ldb-${this.collectionName}-document-${id}`);
         return Promise.resolve({});
     }
 }
