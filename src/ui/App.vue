9ffb0d8129a19f30de828b9556d229e1a07d3771
Ivan Shubin <ivan.ishubin@gmail.com>
2019-06-10 13:44:03 +0200

    removed transition fade

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -5,20 +5,12 @@
 <template>
     <div class="container">
         <div>
-            <transition name="fade">
-                <router-view></router-view>
-            </transition>
+            <router-view></router-view>
         </div>
     </div>
 </template>
 
 <style>
-    .fade-enter-active, .fade-leave-active {
-      transition: opacity .5s
-    }
-    .fade-enter, .fade-leave-active {
-      opacity: 0
-    }
 </style>
 
 <script>
