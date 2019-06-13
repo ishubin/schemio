cb323fcc472eb86cd8576456f96e55fe593c8f23
Ivan Shubin <ivan.ishubin@gmail.com>
2019-06-13 20:58:01 +0200

    Moved all text to foreignObject in svg

--- src/ui/components/editor/items/ComponentItem.vue
+++ src/ui/components/editor/items/ComponentItem.vue
@@ -10,13 +10,16 @@
                 :stroke-width="item.style.stroke.size + 'px'"
                 :fill="item.style.background && item.style.background.color ? item.style.background.color : '#fff'"
             />
-            <text
-                class="item-caption"
-                :x="item.area.x + Math.floor(item.area.w/2 - 15 * item.name.length / (1.75 * 2))"
-                :y="item.area.y + Math.floor(item.area.h / 2)"
-                font-size="15px"
-                :fill="item.style.text && item.style.text.color ? item.style.text.color : '#000'"
-                >{{item.name}}</text>
+            <foreignObject :x="item.area.x" :y="item.area.y" :width="item.area.w" :height="item.area.h">
+                <div style="display: table-cell; vertical-align: middle;" :style="{'width': item.area.w+'px', 'height': item.area.h+'px'}">
+                    <div class="component-item-name-foreign-container" 
+                        :style="{'color': item.style.text && item.style.text.color ? item.style.text.color : '#000'}"
+                        style="width: 100%; text-align: center; display: inline-block"
+                        >
+                        {{item.name}}
+                    </div>
+                </div>
+            </foreignObject>
         </g>
         <g v-else>
             <rect
@@ -27,7 +30,7 @@
                 stroke-width="0"
                 :fill="item.style.background && item.style.background.color ? item.style.background.color : '#fff'"
             />
-            <rect v-if="properties.length > 0 || item.image && item.image.url"
+            <rect v-if="item.properties || item.image && item.image.url"
                 :x="item.area.x"
                 :y="item.area.y + 30"
                 :width="item.area.w"
@@ -35,27 +38,23 @@
                 stroke-width="0"
                 :fill="item.style.properties && item.style.properties.background && item.style.properties.background.color ? item.style.properties.background.color : '#fef'"
             />
-                <text
-                    class="item-caption"
-                    :x="item.area.x + 8"
-                    :y="item.area.y + 20"
-                    font-size="15px"
-                    :fill="item.style.text && item.style.text.color ? item.style.text.color : '#000'"
-                    >{{item.name}}</text>
+                <foreignObject :x="item.area.x" :y="item.area.y" :width="item.area.w" :height="30">
+                    <div class="component-item-name-foreign-container" 
+                        :style="{'color': item.style.text && item.style.text.color ? item.style.text.color : '#000'}">
+                        {{item.name}}
+                    </div>
+                </foreignObject>
 
                 <g v-if="item.image && item.image.url">
                     <image :xlink:href="item.image.url" :x="item.area.x" :y="item.area.y + 30" :width="item.area.w + 'px'" :height="item.area.h - 30 + 'px'"/>
                 </g>
                 <g v-else>
-                    <text v-if="properties.length > 0"
-                        class="item-property"
-                        :x="item.area.x + 4"
-                        :y="item.area.y + 32"
-                        font-size="12px"
-                        :fill="item.style.properties && item.style.properties.text && item.style.properties.text.color ? item.style.properties.text.color : '#666'"
-                        >
-                        <tspan v-for="property in properties" :x="item.area.x + Math.floor(fontsize / 2)" dx="0px" :dy="fontsize" dominant-baseline="alphabetic" style="baseline-shift: 0%;">{{property}}</tspan>
-                    </text>
+                    <foreignObject v-if="item.properties" :x="item.area.x" :y="item.area.y + 32" :width="item.area.w" :height="item.area.h - 32">
+                        <div class="component-item-properties-foreign-container" 
+                            :style="{'color': item.style.properties && item.style.properties.text && item.style.properties.text.color ? item.style.properties.text.color : '#666'}">
+                            <pre>{{item.properties}}</pre>
+                        </div>
+                    </foreignObject>
                 </g>
             <g v-if="item.style.stroke.size > 0">
                 <rect
@@ -68,7 +67,7 @@
                     :stroke-dasharray="strokeDashArray"
                     fill="none"
                 />
-                <path :d="propertiesSeparatorPath" v-if="properties.length > 0 || item.image && item.image.url" stroke-width="1px" :stroke="item.style.stroke.color"/>
+                <path :d="propertiesSeparatorPath" v-if="item.properties || item.image && item.image.url" stroke-width="1px" :stroke="item.style.stroke.color"/>
             </g>
         </g>
     </g>
@@ -112,21 +111,6 @@ export default {
     computed: {
         opacityNumber() {
             return parseFloat(this.item.style.opacity);
-        },
-        properties() {
-            if (this.item.properties.trim().length > 0) {
-                var properties = this.item.properties || '';
-                return _.chain(properties.split('\n')).map(p => {
-                    var v = p.trim();
-                    if (v.length > 0) {
-                        return v;
-                    } else {
-                        return ' ';
-                    }
-                }).value();
-            } else {
-                return [];
-            }
         },
         propertiesSeparatorPath() {
             var x1 = this.item.area.x;
