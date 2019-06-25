5fce8c7253335494c83bb439c7eae0da21669a26
Ivan Shubin <ivan.ishubin@gmail.com>
2019-06-25 22:05:15 +0200

    enabled tests back

--- test/userevents/SetFunction.spec.js
+++ test/userevents/SetFunction.spec.js
@@ -2,68 +2,68 @@ import SetFunction from '../../src/ui/userevents/functions/SetFunction.js';
 import expect from 'expect';
 
 describe('SetFunction', () => {
-    // it('should not fail if item is null', () => {
-    //     SetFunction.execute(null, ['opacity', 1.0]);
-    // });
+    it('should not fail if item is null', () => {
+        SetFunction.execute(null, ['opacity', 1.0]);
+    });
 
-    // it('should not do anything if field is empty', () => {
-    //     const item = { opacity: 0.5 };
-    //     SetFunction.execute(item, ['', 1.0]);
-    //     SetFunction.execute(item, ['.something', 1.0]);
-    //     expect(item).toStrictEqual({opacity: 0.5});
-    // });
+    it('should not do anything if field is empty', () => {
+        const item = { opacity: 0.5 };
+        SetFunction.execute(item, ['', 1.0]);
+        SetFunction.execute(item, ['.something', 1.0]);
+        expect(item).toStrictEqual({opacity: 0.5});
+    });
 
-    // it('should mutate existing property', () => {
-    //     const item = { opacity: 0.5 };
-    //     SetFunction.execute(item, ['opacity', 1.0]);
-    //     expect(item).toStrictEqual({opacity: 1.0});
-    // });
+    it('should mutate existing property', () => {
+        const item = { opacity: 0.5 };
+        SetFunction.execute(item, ['opacity', 1.0]);
+        expect(item).toStrictEqual({opacity: 1.0});
+    });
 
-    // it('should add property', () => {
-    //     const item = { opacity: 0.5 };
-    //     SetFunction.execute(item, ['name', 'Blah']);
-    //     expect(item).toStrictEqual({
-    //         opacity: 0.5,
-    //         name: 'Blah'
-    //     });
-    // });
+    it('should add property', () => {
+        const item = { opacity: 0.5 };
+        SetFunction.execute(item, ['name', 'Blah']);
+        expect(item).toStrictEqual({
+            opacity: 0.5,
+            name: 'Blah'
+        });
+    });
 
-    // it('should mutate existing nested property', () => {
-    //     const item = {
-    //         opacity: 0.5,
-    //         shapeProps: {
-    //             strokeSize: 1,
-    //             someOtherField: {
-    //                 someValue: 123
-    //             }
-    //         }
-    //     };
-    //     SetFunction.execute(item, ['shapeProps.someOtherField.someValue', 123]);
-    //     expect(item).toStrictEqual({
-    //         opacity: 0.5,
-    //         shapeProps: {
-    //             strokeSize: 1,
-    //             someOtherField: {
-    //                 someValue: 123
-    //             }
-    //         }
-    //     });
-    // });
+    it('should mutate existing nested property', () => {
+        const item = {
+            opacity: 0.5,
+            shapeProps: {
+                strokeSize: 1,
+                someOtherField: {
+                    someValue: 123
+                }
+            }
+        };
+        SetFunction.execute(item, ['shapeProps.someOtherField.someValue', 123]);
+        expect(item).toStrictEqual({
+            opacity: 0.5,
+            shapeProps: {
+                strokeSize: 1,
+                someOtherField: {
+                    someValue: 123
+                }
+            }
+        });
+    });
 
-    // it('should extend parent fields for nested property', () => {
-    //     const item = {
-    //         opacity: 0.5
-    //     };
-    //     SetFunction.execute(item, ['shapeProps.someOtherField.someValue', 123]);
-    //     expect(item).toStrictEqual({
-    //         opacity: 0.5,
-    //         shapeProps: {
-    //             someOtherField: {
-    //                 someValue: 123
-    //             }
-    //         }
-    //     });
-    // });
+    it('should extend parent fields for nested property', () => {
+        const item = {
+            opacity: 0.5
+        };
+        SetFunction.execute(item, ['shapeProps.someOtherField.someValue', 123]);
+        expect(item).toStrictEqual({
+            opacity: 0.5,
+            shapeProps: {
+                someOtherField: {
+                    someValue: 123
+                }
+            }
+        });
+    });
 
     it('should not change nested property if it is an object', () => {
         const item = {
