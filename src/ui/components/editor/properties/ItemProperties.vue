f07c6615a1999d5f8599d7b587409b21091ecfcb
Ivan Shubin <ivan.ishubin@gmail.com>
2019-09-22 09:16:38 +0200

    Fixed #52 Not the item properties component does refresh when selecting a new item

--- src/ui/components/editor/properties/ItemProperties.vue
+++ src/ui/components/editor/properties/ItemProperties.vue
@@ -13,12 +13,12 @@
             </li>
         </ul>
 
-        <general-panel :project-id="projectId" v-if="currentTab === 'description'" :item="item"/>
-        <links-panel v-if="currentTab === 'description'" :projectId="projectId" :item="item"/>
-        <connections-panel v-if="currentTab === 'description'" :item="item"/>
-        <position-panel v-if="currentTab === 'position'" :item="item" @ungroup-item="$emit('ungroup-item')"/>
+        <general-panel :key="item.id" :project-id="projectId" v-if="currentTab === 'description'" :item="item"/>
+        <links-panel v-if="currentTab === 'description'" :key="item.id" :projectId="projectId" :item="item"/>
+        <connections-panel v-if="currentTab === 'description'" :key="item.id" :item="item"/>
+        <position-panel v-if="currentTab === 'position'" :key="item.id" :item="item" @ungroup-item="$emit('ungroup-item')"/>
 
-        <behavior-properties v-if="currentTab === 'behavior'" :item="item" :scheme-container="schemeContainer"/>
+        <behavior-properties v-if="currentTab === 'behavior'" :key="item.id" :item="item" :scheme-container="schemeContainer"/>
 
         <div v-if="currentTab === 'shape'">
             <select v-model="item.shape">
@@ -123,6 +123,7 @@ export default {
     },
 
     mounted() {
+        console.log('Mounted');
         this.switchShape(this.item.shape);
     },
 
@@ -166,7 +167,7 @@ export default {
 
         emitItemChanged() {
             EventBus.emitItemChanged(this.item.id);
-        }
+        },
     },
 
     watch: {
