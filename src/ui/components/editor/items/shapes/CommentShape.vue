0ec9c9040912617b382700eb11690dbc16452b79
Ivan Shubin <ivan.ishubin@gmail.com>
2019-12-21 12:45:52 +0100

    Improved comment shape

--- src/ui/components/editor/items/shapes/CommentShape.vue
+++ src/ui/components/editor/items/shapes/CommentShape.vue
@@ -21,9 +21,46 @@ const computePath = (item) => {
     const W = item.area.w;
     const H = item.area.h;
     const R = Math.min(item.shapeProps.cornerRadius, item.area.w/4, item.area.h/4);
-    const TL = Math.min(Math.max(0, item.area.h - item.shapeProps.tailLength), item.shapeProps.tailLength);
-    const TW = Math.min(Math.max(0, item.area.w - item.shapeProps.tailWidth), item.shapeProps.tailWidth);
-    return `M ${W} ${H}  L ${W-TW} ${H-TL}  L ${R} ${H-TL}   a ${R} ${R} 0 0 1 ${-R} ${-R}  L 0 ${R}  a ${R} ${R} 0 0 1 ${R} ${-R}   L ${W-R} 0   a ${R} ${R} 0 0 1 ${R} ${R}  L ${W} ${H} Z`;
+
+    let path = `M 0 ${R} `;
+
+    let sides = [
+        {name: 'top',       length: W - 2*R, vx: 1,     vy: 0,  ax: 1,  ay: -1 },
+        {name: 'right',     length: H - 2*R, vx: 0,     vy: 1,  ax: 1,  ay: 1 },
+        {name: 'bottom',    length: W - 2*R, vx: -1,    vy: 0,  ax: -1, ay: 1 },
+        {name: 'left',      length: H - 2*R, vx: 0,     vy: -1, ax: -1, ay: -1 },
+    ];
+
+    for (let i = 0; i < sides.length; i++) {
+        const side = sides[i];
+        path += `a ${R} ${R} 0 0 1 ${R*side.ax} ${R*side.ay} `;
+        if (item.shapeProps.tailSide === side.name) {
+            const TL = item.shapeProps.tailLength;
+            const TW = Math.min(Math.max(0, side.length - item.shapeProps.tailWidth), item.shapeProps.tailWidth);
+            const t = Math.max(0, Math.min(item.shapeProps.tailPosition, 100.0)) / 100.0;
+            const rotatedX = side.vy;
+            const rotatedY = -side.vx;
+            const p1x = (side.length - TW) * t * side.vx;
+            const p1y = (side.length - TW) * t * side.vy;
+
+            const p2x = TW * t * side.vx + rotatedX * TL;
+            const p2y = TW * t * side.vy + rotatedY * TL;
+
+            const p3x = TW * (1.0 - t) * side.vx - rotatedX * TL;
+            const p3y = TW * (1.0 - t) * side.vy - rotatedY * TL;
+
+            const p4x = (side.length - TW - (side.length - TW) * t) * side.vx;
+            const p4y = (side.length - TW - (side.length - TW) * t) * side.vy;
+
+            path += `l ${p1x} ${p1y} l ${p2x} ${p2y} l ${p3x} ${p3y} l ${p4x} ${p4y} `
+        } else {
+            path += `l ${side.length*side.vx} ${side.length*side.vy} `
+        }
+    }
+
+
+    path = path + ' z';
+    return path;
 }
 
 function identifyTextEditArea(item, itemX, itemY) {
@@ -54,9 +91,11 @@ export default {
         text: 'rich'
     },
     args: {
-        cornerRadius: {type: 'number', value: 20, name: 'Corner radius'},
+        cornerRadius: {type: 'number', value: 10, name: 'Corner radius'},
         tailLength: {type: 'number', value: 30, name: 'Tail Length'},
         tailWidth: {type: 'number', value: 40, name: 'Tail Width'},
+        tailSide: {type: 'choice', value: 'bottom', name: 'Tail Side', options: ['top', 'bottom', 'left', 'right']},
+        tailPosition: {type: 'number', value: 0, name: 'Tail Position', min: 0, max: 100.0},
         strokeColor: {type: 'color', value: 'rgba(100,100,100,1.0)', name: 'Stroke color'},
         strokeSize: {type: 'number', value: 1, name: 'Stroke size'},
         fillColor: {type: 'color', value: 'rgba(230,230,230,1.0)', name: 'Fill color'},
