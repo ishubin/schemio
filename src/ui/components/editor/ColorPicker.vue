14f0fa959308c68ae4534f28fe899167e4639c87
Ivan Shubin <ivan.ishubin@gmail.com>
2020-02-26 14:13:48 +0100

    changed color picker to behave like a tooltip instead of modal

--- src/ui/components/editor/ColorPicker.vue
+++ src/ui/components/editor/ColorPicker.vue
@@ -3,7 +3,7 @@
      file, You can obtain one at https://mozilla.org/MPL/2.0/. -->
 
 <template lang="html">
-    <div class="color-picker">
+    <div class="color-picker" :id="id">
         <div class="color-picker-toggle-button">
             <span class="color-picker-toggle-button-background"></span>
 
@@ -13,27 +13,15 @@
                 ></span>
         </div>
 
-
-        <transition name="modal" v-if="showColorPicker">
-            <div class="modal-mask modal-mask-transparent">
-                <div class="modal-wrapper">
-                    <div class="modal-container" style="width: 300px; background: #fff;">
-                        <div class="modal-body">
-                            <chrome-picker v-model="chromePickerColor" @input="updateColor"></chrome-picker>
-                        </div>
-                        <div class="modal-body">
-                            <span class="btn btn-primary" v-on:click="applyColor()">Ok</span>
-                            <span class="btn btn-secondary" v-on:click="cancelColorPicker()">Cancel</span>
-                        </div>
-                    </div>
-                </div>
-           </div>
-       </transition>
+        <div class="color-picker-tooltip" v-if="tooltip.shown" :style="{left: tooltip.x+'px', top: tooltip.y+'px'}">
+            <chrome-picker v-model="chromePickerColor" @input="updateColor"></chrome-picker>
+        </div>
     </div>
 </template>
 
 <script>
 import VueColor from 'vue-color';
+import shortid from 'shortid';
 
 export default {
     props: {
@@ -43,9 +31,16 @@ export default {
     components: {'chrome-picker': VueColor.Chrome},
     data() {
         return {
+            id: `color-picker-${shortid.generate()}`,
             pickerColor: this.color,
             chromePickerColor: {hex: this.color},
             showColorPicker: false,
+            tooltip: {
+                shown: false,
+                x: 0,
+                y: 0
+            },
+            clickAwayRegistered: false,
             oldColor: null,
             oldAlpha: 1
         }
@@ -54,19 +49,56 @@ export default {
         toggleColorPicker() {
             this.oldColor = this.color;
             this.chromePickerColor = {hex: this.color};
-            this.showColorPicker = true;
+            this.tooltip.shown = true;
+            this.registerClickAwayHandler();
+            this.$nextTick(() => {
+                this.readjustTooltipPosition();
+            });
         },
         updateColor(color) {
             this.pickerColor = color.hex;
             this.$emit('input', `rgba(${color.rgba.r}, ${color.rgba.g}, ${color.rgba.b}, ${color.rgba.a})`);
         },
-        applyColor() {
-            this.showColorPicker = false;
+        readjustTooltipPosition() {
+            const domTooltip = document.querySelector(`#${this.id} .color-picker-tooltip`);
+            const domButton = document.querySelector(`#${this.id} .color-picker-toggle-button`);
+            if (!domTooltip || !domButton) {
+                return;
+            }
+
+            const tooltipRect = domTooltip.getBoundingClientRect();
+            const buttonRect = domButton.getBoundingClientRect();
+            const windowWidth = window.innerWidth;
+            const windowHeight = window.innerHeight;
+            if (buttonRect.left < windowWidth / 2) {
+                this.tooltip.x = buttonRect.right;
+            } else {
+                this.tooltip.x = buttonRect.left - tooltipRect.width;
+            }
+            
+            if (buttonRect.top < windowHeight / 2) {
+                this.tooltip.y = buttonRect.top;
+            } else {
+                this.tooltip.y = buttonRect.bottom - tooltipRect.height;
+            }
         },
-        cancelColorPicker() {
-            this.pickerColor = this.oldColor;
-            this.$emit('input', this.oldColor);
-            this.showColorPicker = false;
+        registerClickAwayHandler() {
+            if (!this.clickAwayRegistered) {
+                document.body.addEventListener('click', this.onGlobalClick);
+                this.clickAwayRegistered = true;
+            }
+        },
+        onGlobalClick(event) {
+            if (!event.target || !event.target.closest(`#${this.id}`)) {
+                this.closeTooltip();
+            }
+        },
+        closeTooltip() {
+            this.tooltip.shown = false;
+            if (this.clickAwayRegistered) {
+                document.body.removeEventListener('click', this.onGlobalClick);
+                this.clickAwayRegistered = false;
+            }
         }
     },
     watch: {
