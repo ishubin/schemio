963e156763900549686e4583f1a290859ee25d21
Ivan Shubin <ivan.ishubin@gmail.com>
2021-04-28 23:31:51 +0200

    fixed path generation for delay uml item

--- src/ui/components/editor/items/shapes/uml/UMLDelay.js
+++ src/ui/components/editor/items/shapes/uml/UMLDelay.js
@@ -4,8 +4,9 @@ function computePath(item) {
     const w = item.area.w;
     const h = item.area.h;
     const r = item.area.h / 2;
-    const x2 = myMath.clamp(w-r, 0)
-    return `M 0 0 L ${x2} 0 A ${r} ${r} 0 0 1 ${x2} ${h} L 0 ${h} Z`;
+    const x2 = myMath.clamp(w-r, 0);
+    const r2 = Math.min(r, w-x2);
+    return `M 0 0 L ${x2} 0 A ${r2} ${r} 0 0 1 ${x2} ${h} L 0 ${h} Z`;
 }
 
 export default {
