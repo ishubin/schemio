6e77869034c2b62853d8a0746e592b87bf386db4
Ivan Shubin <ivan.ishubin@gmail.com>
2021-05-01 12:09:30 +0200

    implemented pins for document uml shape

--- src/ui/components/editor/items/shapes/uml/UMLDocument.js
+++ src/ui/components/editor/items/shapes/uml/UMLDocument.js
@@ -18,6 +18,29 @@ export default {
             iconUrl: '/assets/images/items/uml-document.svg',
         }],
 
+        getPins(item) {
+            const d = item.area.h / 8;
+            const w = item.area.w;
+            const h = item.area.h - d;
+            const k = h / 2;
+            return [{
+                x: w/2, y: h/2,
+            }, {
+                x: w / 2, y: 0,
+                nx: 0, ny: -1
+            }, {
+                x: w / 2, y: h,
+                nx: 0, ny: 1
+            }, {
+                x: 0, y: h/2,
+                nx: -1, ny: 0
+            }, {
+                x: w, y: h/2,
+                nx: 1, ny: 0
+            }];
+
+        },
+
         computePath(item) {
             return computePath(item);
         },
