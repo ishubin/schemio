7e84d9cf3fd6c7ee932e74c7b8004f07c9b4a0c3
Ivan Shubin <ivan.ishubin@gmail.com>
2021-05-24 11:33:10 +0200

    implemented horizontal swim lane support

--- src/ui/components/editor/items/shapes/uml/UMLSwimLane.js
+++ src/ui/components/editor/items/shapes/uml/UMLSwimLane.js
@@ -2,31 +2,61 @@ import myMath from '../../../../../myMath';
 import AdvancedFill from '../../AdvancedFill.vue';
 import {enrichItemTextSlotWithDefaults} from '../../../../../scheme/Item';
 
+function swimLaneWidth(item) {
+    if (item.shapeProps.vertical) {
+        return item.area.w;
+    }
+    return item.area.h;
+}
+
+function swimLaneHeight(item) {
+    if (item.shapeProps.vertical) {
+        return item.area.h;
+    }
+    return item.area.w;
+}
+
 function computeOutline(item) {
     return `M 0 0 L ${item.area.w} 0 L ${item.area.w} ${item.area.h} L 0 ${item.area.h} Z`;
 }
 
 function computeCurves(item) {
-    const headerHeight = myMath.clamp(item.shapeProps.headerHeight, 0, item.area.h);
+    const headerHeight = myMath.clamp(item.shapeProps.headerHeight, 0, swimLaneHeight(item));
+
+    let headerLinePath = '';
+    if (item.shapeProps.vertical) {
+        headerLinePath = `M 0 ${headerHeight} L ${item.area.w} ${headerHeight}`;
+    } else {
+        headerLinePath = `M ${headerHeight} 0 L ${headerHeight} ${item.area.h}`;
+    }
+
     const curves = [{
         path: computeOutline(item),
         fill: AdvancedFill.computeStandardFill(item),
         strokeColor: item.shapeProps.strokeColor,
         strokeSize: item.shapeProps.strokeSize,
     }, {
-        path: `M 0 ${headerHeight} L ${item.area.w} ${headerHeight}`,
+        path: headerLinePath,
         fill: 'none',
         strokeColor: item.shapeProps.strokeColor,
         strokeSize: item.shapeProps.strokeSize,
     }];
+
     
     let previousColumnRatio = 0;
 
     for (let i = 1; i < item.shapeProps.columns; i++) {
         let columnRatio = myMath.clamp(previousColumnRatio + Math.abs(item.shapeProps[`colw${i}`]), 0, 100);
-        const pos = columnRatio * item.area.w / 100.0;
+        const pos = columnRatio * swimLaneWidth(item) / 100.0;
+
+        let path = '';
+        if (item.shapeProps.vertical) {
+            path = `M ${pos} 0 L ${pos} ${swimLaneHeight(item)}`;
+        } else {
+            path = `M 0 ${pos} L ${swimLaneHeight(item)} ${pos}`;
+        }
         curves.push({
-            path: `M ${pos} 0 L ${pos} ${item.area.h}`,
+            path,
             fill: 'none',
             strokeColor: item.shapeProps.strokeColor,
             strokeSize: item.shapeProps.strokeSize,
@@ -46,38 +76,59 @@ function makeColumnControlPoint(item, columnNumber) {
     let offset = 0;
     for (let i = 1; i <= columnNumber; i++) {
         const r = item.shapeProps[`colw${i}`];
-        offset += r * item.area.w / 100.0;
+        offset += r * swimLaneWidth(item) / 100.0;
     }
     
-    return {
-        x: myMath.clamp(offset, 0, item.area.w),
-        y: 0
+    if (item.shapeProps.vertical) {
+        return {
+            x: myMath.clamp(offset, 0, swimLaneWidth(item)),
+            y: 0
+        };
+    } else {
+        return {
+            x: 0,
+            y: myMath.clamp(offset, 0, swimLaneWidth(item))
+        };
     }
 }
 
 
 function getTextSlots(item) {
     const textSlots = [];
+    const W = swimLaneWidth(item);
+
     let previousColumnRatio = 0;
     let previousPosition = 0;
     for (let i = 1; i <= item.shapeProps.columns; i++) {
         let columnRatio = myMath.clamp(previousColumnRatio + Math.abs(item.shapeProps[`colw${i}`]), 0, 100);
-        const pos = columnRatio * item.area.w / 100.0;
+        const pos = columnRatio * W / 100.0;
 
         let width = pos - previousPosition;
         if (i === item.shapeProps.columns) {
-            width = item.area.w - previousPosition;
+            width = W - previousPosition;
         }
 
-        textSlots.push({
-            name: `col${i}`,
-            area: {
-                x: previousPosition,
-                y: 0,
-                w: width,
-                h: item.shapeProps.headerHeight
-            }
-        });
+        if (item.shapeProps.vertical) {
+            textSlots.push({
+                name: `col${i}`,
+                area: {
+                    x: previousPosition,
+                    y: 0,
+                    w: width,
+                    h: item.shapeProps.headerHeight
+                }
+            });
+        } else {
+            textSlots.push({
+                name: `col${i}`,
+                area: {
+                    x: 0,
+                    y: previousPosition,
+                    w: item.shapeProps.headerHeight,
+                    h: width
+                }
+            });
+        }
         previousColumnRatio = columnRatio;
         previousPosition = pos;
     }
@@ -87,23 +138,31 @@ function getTextSlots(item) {
 
 function onColumnNumberInput(item, columns, previousColumns) {
     const columnWidths = [];
-    let totalColumnSum = 0
+    let totalColumnSum = 0;
+    let W = swimLaneWidth(item);
+
     for (let i = 1; i < previousColumns; i++) {
-        const width = item.shapeProps[`colw${i}`] * item.area.w / 100.0;
+        const width = item.shapeProps[`colw${i}`] * W / 100.0;
         columnWidths.push(width);
         totalColumnSum += width;
     }
 
-    if (columns > previousColumns && columns > 1 && !myMath.tooSmall(item.area.w)) {
+    if (columns > previousColumns && columns > 1 && !myMath.tooSmall(W)) {
         // adding a column
-        const columnWidth = item.area.w  / (columns - 1);
+        const columnWidth = W  / (columns - 1);
 
-        item.area.w += columnWidth;
+        if (item.shapeProps.vertical) {
+            item.area.w += columnWidth;
+        } else {
+            item.area.h += columnWidth;
+        }
 
-        item.shapeProps[`colw${columns-1}`] = 100 * columnWidth / item.area.w;
+        W = swimLaneWidth(item);
+
+        item.shapeProps[`colw${columns-1}`] = 100 * columnWidth / W;
 
         for (let i = 1; i < previousColumns; i++) {
-            item.shapeProps[`colw${i}`] = 100 * columnWidths[i-1] / item.area.w;
+            item.shapeProps[`colw${i}`] = 100 * columnWidths[i-1] / W;
         }
 
         if (!item.textSlots[`col${columns}`]) {
@@ -114,11 +173,18 @@ function onColumnNumberInput(item, columns, previousColumns) {
         }
     }
 
-    if (columns < previousColumns && columns > 0 && !myMath.tooSmall(item.area.w)) {
-        item.area.w = totalColumnSum;
-        if (!myMath.tooSmall(item.area.w)) {
+    if (columns < previousColumns && columns > 0 && !myMath.tooSmall(W)) {
+        if (item.shapeProps.vertical) {
+            item.area.w = totalColumnSum;
+        } else {
+            item.area.h = totalColumnSum;
+        }
+
+        W = swimLaneWidth(item);
+
+        if (!myMath.tooSmall(W)) {
             for (let i = 1; i < columns; i++) {
-                item.shapeProps[`colw${i}`] = 100 * columnWidths[i-1] / item.area.w;
+                item.shapeProps[`colw${i}`] = 100 * columnWidths[i-1] / W;
             }
         }
     }
@@ -138,6 +204,25 @@ export default {
             item: {
                 shapeProps: {
                     columns: 3,
+                    colw1: 33.33333,
+                    colw2: 33.33333,
+                    colw3: 33.33333,
+                },
+                textSlots: {
+                    col1: { text: '<b>Todo</b>'},
+                    col2: { text: '<b>In Progress</b>'},
+                    col3: { text: '<b>Done</b>'},
+                }
+            }
+        }, {
+            group: 'UML',
+            name: 'Swim Lane (Horizontal)',
+            iconUrl: '/assets/images/items/uml-swim-lane-horizontal.svg',
+            item: {
+                shapeProps: {
+                    columns: 3,
+                    vertical: false,
+                    headerHeight: 80,
                     colw1: 33.33333,
                     colw2: 33.33333,
                     colw3: 33.33333,
@@ -168,11 +253,19 @@ export default {
         controlPoints: {
             make(item, pointId) {
                 if (!pointId) {
-                    const cps = {
-                        headerHeight: {
+                    const cps = { };
+
+                    if (item.shapeProps.vertical) {
+                        cps.headerHeight = {
                             x: 0, y: myMath.clamp(item.shapeProps.headerHeight, 0, item.area.h)
-                        }
-                    };
+                        };
+                    } else {
+                        cps.headerHeight = {
+                            x: myMath.clamp(item.shapeProps.headerHeight, 0, item.area.w), y: 0
+                        };
+                    }
+
+
                     // doing it in reverse order so that control point for first column would always be rendered on top of second control point
                     for (let i = item.shapeProps.columns - 1; i > 0; i--) {
                         cps[`colw${i}`] = makeColumnControlPoint(item, i);
@@ -185,32 +278,44 @@ export default {
             },
             handleDrag(item, controlPointName, originalX, originalY, dx, dy) {
                 if (controlPointName.indexOf('colw') === 0) {
+                    let movement = originalX + dx;
+                    if (!item.shapeProps.vertical) {
+                        movement = originalY + dy;
+                    }
+
+                    const W = swimLaneWidth(item);
 
                     const columns = Math.max(1, item.shapeProps.columns);
-                    if (myMath.tooSmall(item.area.w)) {
+                    if (myMath.tooSmall(W)) {
                         return;
                     }
                     const columnNumber = parseInt(controlPointName.substr(4));
                     let offset = 0;
                     for (let i = 1; i < columnNumber; i++) {
                         const r = item.shapeProps[`colw${i}`];
-                        offset += r * item.area.w / 100.0;
+                        offset += r * W / 100.0;
                     }
                     
-                    const width = Math.max(0, originalX + dx - offset);
+                    const width = Math.max(0, movement - offset);
 
                     const minWidth = 5 / columns;
                     const maxWidth = 100 - 5 / columns;
-                    item.shapeProps[`colw${columnNumber}`] = myMath.clamp(100 * width / item.area.w, minWidth, maxWidth);
+                    item.shapeProps[`colw${columnNumber}`] = myMath.clamp(100 * width / W, minWidth, maxWidth);
+
                 } else if (controlPointName === 'headerHeight') {
-                    item.shapeProps.headerHeight = myMath.clamp(originalY + dy, 0, item.area.h);
+                    let movement = originalY + dy;
+                    if (!item.shapeProps.vertical) {
+                        movement = originalX + dx;
+                    }
+                    item.shapeProps.headerHeight = myMath.clamp(movement, 0, swimLaneHeight(item));
                 }
             }
         },
         
         args: {
             columns: {type: 'number', value: 3, name: 'Columns', min: 1, max: 12, onInput: onColumnNumberInput },
-            headerHeight: {type: 'number', value: 60, name: 'Header Height', min: 0, hidden: true},
+            headerHeight: {type: 'number', value: 50, name: 'Header Height', min: 0, hidden: true},
+            vertical: {type: 'boolean', value: true, name: 'Vertical'},
             colw1: {type: 'number', value: 20, name: 'Column Width 1', min: 0, max: 100.0, hidden: true},
             colw2: {type: 'number', value: 20, name: 'Column Width 2', min: 0, max: 100.0, hidden: true},
             colw3: {type: 'number', value: 20, name: 'Column Width 3', min: 0, max: 100.0, hidden: true},
