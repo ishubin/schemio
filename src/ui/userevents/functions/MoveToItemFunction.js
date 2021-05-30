c014ad087e66c9854ed906ba72f8f7044ca5664d
Ivan Shubin <ivan.ishubin@gmail.com>
2021-05-30 11:33:52 +0200

    #414 added rotation aligning in "move to item"

--- src/ui/userevents/functions/MoveToItemFunction.js
+++ src/ui/userevents/functions/MoveToItemFunction.js
@@ -9,7 +9,7 @@ function calculateItemPositionToMatchAnotherItem(item, destinationItem, schemeCo
 }
 
 class MoveToItemAnimation extends Animation {
-    constructor(item, args, destinationPosition, schemeContainer, resultCallback) {
+    constructor(item, args, destinationPosition, destinationAngle, schemeContainer, resultCallback) {
         super();
         this.item = item;
         this.args = args;
@@ -21,6 +21,8 @@ class MoveToItemAnimation extends Animation {
             y: this.item.area.y
         };
         this.destinationPosition = destinationPosition;
+        this.originalAngle = item.area.r;
+        this.destinationAngle = destinationAngle;
     }
 
     init() {
@@ -33,20 +35,24 @@ class MoveToItemAnimation extends Animation {
 
             const t = Math.min(1.0, this.elapsedTime / (this.args.duration * 1000));
 
-            if (t >= 1.0){
-                this.item.area.x = this.destinationPosition.x;
-                this.item.area.y = this.destinationPosition.y;
-                this.schemeContainer.reindexItemTransforms(this.item);
-                return false;
-            }
+            let shouldProceedAnimating = true;
+            
+            let convertedT = convertTime(t, this.args.movement);
 
-            const convertedT = convertTime(t, this.args.movement);
+            if (t >= 1.0){
+                convertedT = 1.0;
+                shouldProceedAnimating = false;
+            }
 
             this.item.area.x = this.originalPosition.x * (1.0 - convertedT) + this.destinationPosition.x * convertedT;
             this.item.area.y = this.originalPosition.y * (1.0 - convertedT) + this.destinationPosition.y * convertedT;
+
+            if (this.args.rotate) {
+                this.item.area.r = this.originalAngle * (1.0 - convertedT) + this.destinationAngle * convertedT;
+            }
             this.schemeContainer.reindexItemTransforms(this.item);
 
-            return true;
+            return shouldProceedAnimating;
         } else {
             this.item.area.x = this.destinationPosition.x;
             this.item.area.y = this.destinationPosition.y;
@@ -70,21 +76,34 @@ export default {
         destinationItem : {name: 'Destination Item',  type: 'element',value: null, description: 'Other item to which this item should be moved'},
         animate         : {name: 'Animate',           type: 'boolean',value: false},
         duration        : {name: 'Duration (sec)',    type: 'number', value: 2.0, depends: {animate: true}},
-        movement        : {name: 'Movement',          type: 'choice', value: 'linear', options: ['linear', 'smooth', 'ease-in', 'ease-out', 'ease-in-out', 'bounce'], depends: {animate: true}},
+        movement        : {name: 'Movement',          type: 'choice', value: 'ease-in-out', options: ['linear', 'smooth', 'ease-in', 'ease-out', 'ease-in-out', 'bounce'], depends: {animate: true}},
+        rotate          : {name: 'Rotate',            type: 'boolean',value: false, description: 'Align rotation of items'},
+        rotationOffset  : {name: 'Rotation Offset',   type: 'number', value: 0.0, depends: {rotate: true}, description: 'Rotation angle offset'},
         inBackground    : {name: 'In Background',     type: 'boolean',value: false, description: 'Play animation in background without blocking invokation of other actions', depends: {animate: true}}
     },
 
     execute(item, args, schemeContainer, userEventBus, resultCallback) {
         if (item) {
             const destinationItem = schemeContainer.findFirstElementBySelector(args.destinationItem, item);
+            let destinationAngle = 0;
             let destinationPosition = null;
             if (destinationItem && destinationItem.id !== item.id) {
                 destinationPosition = calculateItemPositionToMatchAnotherItem(item, destinationItem, schemeContainer);
+
+                let worldAngle = destinationItem.area.r;
+                if (destinationItem.meta && destinationItem.meta.transform) {
+                    worldAngle += destinationItem.meta.transform.r;
+                }
+
+                if (item.meta && item.meta.transform) {
+                    destinationAngle = worldAngle - item.meta.transform.r;
+                }
             }
+
             
             if (destinationPosition) {
                 if (args.animate) {
-                    AnimationRegistry.play(new MoveToItemAnimation(item, args, destinationPosition, schemeContainer, resultCallback), item.id);
+                    AnimationRegistry.play(new MoveToItemAnimation(item, args, destinationPosition, destinationAngle, schemeContainer, resultCallback), item.id);
                     if (args.inBackground) {
                         resultCallback();
                     }
@@ -93,6 +112,9 @@ export default {
                 } else {
                     item.area.x = destinationPosition.x;
                     item.area.y = destinationPosition.y;
+                    if (args.rotate) {
+                        item.area.r = destinationAngle;
+                    }
                     schemeContainer.reindexItemTransforms(item);
                 }
             }
