bf95709c6648d3b8c67b0fc514d9e66e6fbdbd45
Ivan Shubin <ivan.ishubin@gmail.com>
2021-07-06 16:33:57 +0200

    minor change

--- src/ui/views/AboutView.vue
+++ src/ui/views/AboutView.vue
@@ -4,9 +4,12 @@
         <div class="middle-content">
             <h2>About Schemio</h2>
 
-            <a href="/">Schmeio</a> is an in-browser diagrams drawing app that can also be used for interface prototyping.
-            It was designed with the idea that any complex solution can be mapped to a simple diagram and the complexity of this solution could be uncovered step by step 
-            with the help of links and dynamic behavior.
+            <p>
+                <a href="/">Schmeio</a> is an in-browser diagrams drawing app that can also be used for interface prototyping.
+                It was designed with an idea that any complex solution can be mapped to a simple diagram
+                and the complexity of this solution could be uncovered step by step with the help of links and dynamic behavior.
+            </p>
+
         </div>
     </div>
 </template>
