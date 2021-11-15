f66384b12ab3f96d1648d8009790cf7e4803545a
Ivan Shubin <ivan.ishubin@gmail.com>
2021-11-15 19:22:11 +0100

    minor fixes

--- src/ui/standalone-viewer.js
+++ src/ui/standalone-viewer.js
@@ -27,7 +27,7 @@ window.schemioViewScheme = (elementOrSelector, scheme, opts) => {
                 autoZoom: objProperty(options, 'autoZoom', true),
                 sidePanelWidth: parseInt(options.sidePanelWidth) || 400,
                 useMouseWheel: objProperty(options, 'useMouseWheel', false),
-                homeLink: options.homeLink || '/'
+                homeLink: 'https://github.com/ishubin/schemio'
             };
         },
         template: '<standalone-scheme-view :scheme="scheme" :offset-x="offsetX" :offset-y="offsetY" :zoom="zoom" :auto-zoom="autoZoom" :home-link="homeLink" :side-panel-width="sidePanelWidth" :use-mouse-wheel="useMouseWheel"/>'
