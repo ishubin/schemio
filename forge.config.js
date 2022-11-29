b374db08d6ac287a86bf386bdcc38da4776c9da6
Ivan Shubin <ivan.ishubin@gmail.com>
2022-11-29 20:26:39 +0100

    added app icon for electron

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    icon: 'design/app-icons/icons/all/icon'
+  },
   rebuildConfig: {},
   makers: [
     {
---
+++ generate-electron-app-icons.sh
@@ -0,0 +1,4 @@
+./node_modules/.bin/electron-icon-maker --input=design/app-icons/schemio-app-icon.png --output=design/app-icons/
+cp design/app-icons/icons/win/icon.ico design/app-icons/icons/all/.
+cp design/app-icons/icons/mac/icon.icns design/app-icons/icons/all/.
+cp design/app-icons/icons/png/1024x1024.png design/app-icons/icons/all/icon.png
