49c47888c14e810d5aaa2e188a9271e39a6b7e31
Ivan Shubin <ivan.ishubin@gmail.com>
2022-12-06 21:40:53 +0100

    ci: fix upload of drive app

--- scripts/ci/upload-drive-app-to-server.sh
+++ scripts/ci/upload-drive-app-to-server.sh
@@ -6,4 +6,4 @@ mkdir -p ~/.ssh/
 echo "$DRIVE_SERVER_KEY" > ~/.ssh/driveserver.key
 sudo chmod 600 ~/.ssh/driveserver.key
 
-rsync -e "ssh -o StrictHostKeyChecking=no" dist/drive-app "$DRIVE_SERVER_ENDPOINT"
+rsync -r -e "ssh -i ~/.ssh/driveserver.key -o StrictHostKeyChecking=no" dist/drive-app "$DRIVE_SERVER_USER@$DRIVE_SERVER_HOST:$DRIVE_SERVER_PATH"
