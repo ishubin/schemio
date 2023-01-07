9e03a793ae84f4f5a4a9b816ab361b86aee75ce5
Ivan Shubin <ivan.ishubin@gmail.com>
2023-01-07 13:51:40 +0100

    fixed reseting of failed component

--- src/ui/components/editor/items/shapes/Component.vue
+++ src/ui/components/editor/items/shapes/Component.vue
@@ -40,7 +40,7 @@
             <div class="progress-bar" :style="progressBarStyle"></div>
         </foreignObject>
 
-        <g @click="resetFailureMessage" @touchstart="resetFailureMessage" v-if="!isLoading && item.meta && item.meta.componentLoadFailed" style="cursor: pointer;">
+        <g v-if="!isLoading && item.meta && item.meta.componentLoadFailed" style="cursor: pointer;">
             <rect  :x="0" :y="0" :width="item.area.w" :height="item.area.h" fill="rgba(250, 70, 70)"/>
             <foreignObject :x="0" :y="0" :width="item.area.w" :height="item.area.h" >
                 <div class="item-text-container" :style="failureMessageStyle" xmlns="http://www.w3.org/1999/xhtml"><b>Loading failed</b></div>
@@ -271,6 +271,9 @@ export default {
         },
 
         computeCustomAreas(item) {
+            if (item.meta && item.meta.componentLoadFailed) {
+                return [];
+            }
             return [{
                 id: 'load-button',
                 cursor: 'pointer',
@@ -407,7 +410,9 @@ export default {
             }
         },
         onMouseDown(customAreaId) {
-            if (customAreaId === 'load-button') {
+            if (!this.isLoading && this.item.meta && this.item.meta.componentLoadFailed) {
+                this.resetFailureMessage();
+            } else if (customAreaId === 'load-button') {
                 this.onLoadSchemeClick();
             }
         },
