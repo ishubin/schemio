903d6a626bfa0411cd6453266f0a483b4bfdf02f
Ivan Shubin <ivan.ishubin@gmail.com>
2024-04-07 06:55:41 +0200

    added possibility to import scripts from a file in yaml template

--- build-template-index.py
+++ build-template-index.py
@@ -21,6 +21,26 @@ def write_json_to_file(data, file_path):
         json.dump(data, f)
 
 
+def read_file(file_path):
+    data = None
+    with open(file_path, 'r') as file:
+        data = file.read()
+    return data
+
+
+
+# Only used for yaml templates
+def process_template(template, template_path):
+    dir_name = os.path.dirname(template_path)
+    if 'import' in template:
+        init_script_prefix = ''
+        for import_path in template['import']:
+            imported_script = read_file(os.path.join(dir_name, import_path))
+            init_script_prefix += imported_script + '\n\n'
+        template['init'] = init_script_prefix + template['init']
+
+
+
 def index_template_item(index, file_path):
     if not os.path.isfile(file_path):
         return
@@ -37,6 +57,7 @@ def index_template_item(index, file_path):
     elif file_path.endswith('.yaml'):
         template = load_yaml_from_file(file_path)
         resulting_file_path = file_path[0:extension_idx] + '.json'
+        process_template(template, file_path)
         write_json_to_file(template, resulting_file_path)
 
 
